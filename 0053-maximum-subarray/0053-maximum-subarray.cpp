5dd7acc24a9ac5c232f492b198512466adf6d972
Subha Sankar Sahu <subhasankarsahu5@gmail.com>
2026-08-21 01:43:53 +0530

    Time: 0 ms (100%), Space: 71.8 MB (15.04%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <algorithm>
+#include <climits>
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSum = INT_MIN;
+        int currentSum = 0;
+
+        for (int num : nums){
+            currentSum += num;
+            maxSum = std::max(maxSum, currentSum);
+            if(currentSum < 0){
+                currentSum = 0;
+            }
+        }
+        return maxSum;
+    }
+};
